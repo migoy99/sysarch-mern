560fbf031c00567eb432141c7078130eef42722e
Miguel Trinidad <migfrantrinidad@gmail.com>
2023-05-08 19:10:56 +0800

    update

--- src/components/NavBarLogged.jsx
+++ src/components/NavBarLogged.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
